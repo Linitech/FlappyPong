06109fc207e1f10bc5fbbc73399fbf3dcaae62dc
Linitech <zela6426@gmail.com>
2024-04-06 12:53:31 -0400

    created the physics

--- szhu_midterm_project/Assets/Script/Bird.cs
+++ szhu_midterm_project/Assets/Script/Bird.cs
@@ -5,27 +5,54 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    private Vector2 direction;
+    private float direction = 1;
 
     bool goingUp;
     bool goingDown;
 
+    private Rigidbody2D rb2d;
+
+    public bool xMove = true;
+    private float speed;
+
+    public float Border { get; private set; }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>(); //declare value of the variable
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+
+        //var velocity = rb2d.velocity; //creates and declare variables for velocity
+
+        if (Input.GetKey(KeyCode.W))
         {
-            direction = Vector2.up;
+            rb2d.AddForce(Vector2.up * direction);
         }
 
-        transform.position = new Vector2
-        ((transform.position.x + direction.x), transform.position.y + direction.y);
+        if (xMove == true)
+        {
+            transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+        }
+        if (transform.position.x >= Border)
+        {
+            xMove = false;
+        }
+        else if (transform.position.x <= -Border)
+        {
+            xMove = true;
+        }
+
+
 
     }
 }
